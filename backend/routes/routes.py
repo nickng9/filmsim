c77c3ffcee062421c43ffb48bb1180fe36f83ddf
Alan Zhang <alan.zhang.jedi@gmail.com>
2024-06-28 23:05:24 -0400

    able to process picture from frontend now

--- backend/routes/routes.py
+++ backend/routes/routes.py
@@ -3,11 +3,9 @@ from PIL import Image
 import sys
 from flask_cors import CORS, cross_origin
 import os
-import json
 
-# sys.path.append(os.path.abspath(os.path.join(os.path.pardir, 'filter')))
-# sys.path.insert(0, '/Users/alan/Documents/film-filter-project/backend/filter.py')
-# from filter import applyNoise
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from filter import applyNoise
 
 app = Flask(__name__)
 # CORS(app, resources={r'/*': {'origins': 'http://localhost:3000'}}, supports_credentials=True)
@@ -16,11 +14,11 @@ CORS(app)
 # haldCut = Image.open('Kodak Portra 400 1 -.png')
 # imageTwo = Image.open('IMG_4764.JPG')
 
-@app.route('/')
-def helloWorld():
-    return 'Hello World'
-    # image = applyNoise(haldCut, imageTwo)
-    # return image
+# @app.route('/')
+# def helloWorld():
+#     return 'Hello World'
+#     # image = applyNoise(haldCut, imageTwo)
+#     # return image
 
 @app.route('/processPicture', methods=['POST', 'OPTIONS'])
 @cross_origin()
@@ -30,8 +28,15 @@ def processPicture():
         return jsonify({'error': 'No picture part in the request'}), 400
 
     img = request.files['picture']
+
     if img.filename == '':
         return jsonify({'error': 'No selected file'}), 400
+    
+    haldClut = 'backend/routes/Kodak Portra 400 1 -.png'
+    img = applyNoise(haldClut, img.stream)
+    img.show()
+    app.logger.info(img)
+
     response = jsonify({'picture': 'sack!'})
 
     return response
