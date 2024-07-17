6b925b24f6a7211d1cafd0b7259874a47f9e0d0b
Nicholas Ng <nicholas.m.ng@gmail.com>
2024-07-17 18:57:46 -0400

    navbar hover state + uppercase

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -1,24 +1,52 @@
-// src/app/components/Navbar.tsx
-"use client";
+'use client';
 
 import React from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import styles from '../../styles/Navbar.module.css';
 
-const Navbar: React.FC = () => {
+const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
-        <Link href="/">FILMOGRAPHY</Link>
+        <Link href="/" className={pathname === '/' ? `${styles.navLink} ${styles.active}` : styles.navLink}>
+          FILMOGRAPHY
+        </Link>
       </div>
-      <div className={styles.navLinks}>
-        <Link href="/">Home</Link>
-        <Link href="/upload">Upload</Link>
-        <Link href="/catalogue">Catalogue</Link>
-        <Link href="/community">Community</Link>
-        <Link href="/contact">Contact</Link>
-        <Link href="/account">My Account</Link>
-      </div>
+      <ul className={styles.navLinks}>
+        <li>
+          <Link href="/" className={pathname === '/' ? `${styles.navLink} ${styles.active}` : styles.navLink}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/upload" className={pathname === '/upload' ? `${styles.navLink} ${styles.active}` : styles.navLink}>
+            Upload
+          </Link>
+        </li>
+        <li>
+          <Link href="/catalogue" className={pathname === '/catalogue' ? `${styles.navLink} ${styles.active}` : styles.navLink}>
+            Catalogue
+          </Link>
+        </li>
+        <li>
+          <Link href="/community" className={pathname === '/community' ? `${styles.navLink} ${styles.active}` : styles.navLink}>
+            Community
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact" className={pathname === '/contact' ? `${styles.navLink} ${styles.active}` : styles.navLink}>
+            Contact
+          </Link>
+        </li>
+        <li>
+          <Link href="/account" className={pathname === '/account' ? `${styles.navLink} ${styles.active}` : styles.navLink}>
+            My Account
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
