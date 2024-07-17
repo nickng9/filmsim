38f5ed01eb7d2a24b025ae0fdf99aae7e79b546a
Nicholas Ng <nicholas.m.ng@gmail.com>
2024-07-17 18:58:50 -0400

    fixing layout.tsx again

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,10 +3,10 @@ import './globals.css';
 import Navbar from './components/Navbar';
 
 import React, { ReactNode } from 'react';
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <head />
       <body>
         <Navbar />
         {children}
