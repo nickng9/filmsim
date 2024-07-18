1e1d3191b932601fc708ca2993aa930abb63ea00
Nicholas Ng <nicholas.m.ng@gmail.com>
2024-07-18 00:51:43 -0400

    adding in randomized positioning of photos + arrow vectors

--- frontend/src/app/components/UploadButton.tsx
+++ frontend/src/app/components/UploadButton.tsx
@@ -1,4 +1,3 @@
-// src/app/components/UploadButton.tsx
 "use client";
 
 import React from 'react';
@@ -7,10 +6,14 @@ import styles from '../../styles/UploadButton.module.css';
 
 const UploadButton: React.FC = () => {
   return (
-    <div className={styles.uploadButton}>
-      <Link href="/upload" legacyBehavior>
-        <a className={styles.button}>Upload a Photo to Get Started</a>
-      </Link>
+    <div className={styles.uploadContainer}>
+      <img src="/images/RightFacingArrow.svg" alt="Left Arrow" className={styles.arrow} />
+      <div className={styles.uploadButton}>
+        <Link href="/upload" legacyBehavior>
+          <a className={styles.button}>Upload a Photo to Get Started</a>
+        </Link>
+      </div>
+      <img src="/images/LeftFacingArrow.svg" alt="Right Arrow" className={styles.arrow} />
     </div>
   );
 };
